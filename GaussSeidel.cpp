ebf7054dcfdb37ce55c04664f0b70e2f4d8f5470
diego <136401880+DKB-17@users.noreply.github.com>
2024-06-20 20:41:12 -0300

    feat: completa

--- GaussSeidel.cpp
+++ GaussSeidel.cpp
@@ -5,66 +5,7 @@
 
 using namespace std;
 
-double parada(float v[], float x[], int tam){
-
-    float maxnum = 0;
-    float maxden = 0;
-    for(int i=0; i < tam;i++){
-        int num = abs(v[i] - x[i]);
-        if(num > maxnum){
-            maxnum = num;
-        }
-        int den = abs(v[i]);
-        if(den > maxden){
-            maxden = den;
-        }
-    }
-    return (double)maxnum/maxden;
-}
-
-void gaussSeidel (float A[][ordemMax], float b[], float e, int tam){
-
-    int interMax = 50;
-    float x[tam];
-    float v[tam];
-
-    for(int i=0;i < tam;i++){
-        x[i] = b[i]/A[i][i];
-        v[i] = 0;
-    }
-    for(int k=1;k<interMax;k++){
-            cout << "\n\t" << k << " Interacao: ";
-            for(int i=0;i<tam;i++){
-                cout << x[i] << "|";
-            }
-        for(int i = 0; i<tam; i++){
-            float S = 0;
-            for(int j=0; j<tam; j++){
-                if(i != j){
-                    S += A[i][j] * x[j];
-                }
-            }
-            //cout << "\n" << b[i] << "-" << S << "/" << A[i][i] << "=" << ((b[i] - S) / A[i][i]);
-            x[i] = (float) ((b[i] - S) / A[i][i]);
-        }
-        double d = parada(x, v, tam);
-        if (d < e){
-                cout << endl;
-            for(int i=0;i<tam;i++){
-                cout << x[i] << "|";
-            }
-            return;
-        }
-
-        for(int i=0;i<tam;i++){
-            v[i] = x[i];
-        }
-    }
-
-
-}
-
-void lerVetor(float b[], int ordem){
+void lerVetor(double b[], int ordem){
     cout << endl;
     for(int j=0;j<ordem;j++){
         cout << "\tx[" << (j+1) << "]: ";
@@ -72,29 +13,29 @@ void lerVetor(float b[], int ordem){
     }
 }
 
-void lerMatriz(float A[][ordemMax], int ordem){
+void lerMatriz(double A[][ordemMax], int ordem){
     for(int i=0;i<ordem;i++){
         cout << "\n\t" << (i+1) << " Equacao:";
         lerVetor(A[i], ordem);
     }
 }
 
-void imprimirVetor(float b[], int ordem){
+void imprimirVetor(double b[], int ordem){
     cout << endl;
     for(int j=0; j<ordem; j++){
         cout << "\t" ;
         if(j>0){
             if(b[j]<0){
-                cout << "- ";
+                cout << " ";
             }else{
-                cout << "+ ";
+                cout << "+";
             }
         }
         cout << b[j] << "x[" << (j+1) << "]";
     }
 }
 
-void imprimirSistema(float A[][ordemMax], float b[], int ordem){
+void imprimirSistema(double A[][ordemMax], double b[], int ordem){
     for(int i=0;i<ordem;i++){
         imprimirVetor(A[i], ordem);
         cout << "\t= " << b[i];
@@ -106,16 +47,16 @@ void lerErro(float *e){
     cin >> *e;
 }
 
-bool converge(float A[][ordemMax], int tam){
+bool converge(double A[][ordemMax], int tam){
 
     double b[tam];
     for(int i=0; i<tam; i++){
         b[i] = 1;
     }
-    float maxnum = 0;
+    double maxnum = 0;
     for(int i=0; i < tam;i++){
-        float den = A[i][i];
-        float num = 0;
+        double den = A[i][i];
+        double num = 0;
         for(int j=0;j < tam;j++){
             if(i != j){
                 num += (A[i][j] * b[j]);
@@ -133,12 +74,72 @@ bool converge(float A[][ordemMax], int tam){
     }
 }
 
+double parada(double v[], double x[], int tam){
+
+    double maxnum = 0.0;
+    double maxden = 0.0;
+    for(int i=0; i < tam;i++){
+        float num = (v[i] - x[i]);
+        if(num > maxnum){
+            maxnum = num;
+        }
+        float den = abs(v[i]);
+        if(den > maxden){
+            maxden = den;
+        }
+    }
+    return (maxnum/maxden);
+}
+
+void gaussSeidel (double A[][ordemMax], double b[], float e, int tam){
+
+    int interMax = 50;
+    double x[tam];
+    double v[tam];
+
+    for(int i=0;i < tam;i++){
+        x[i] = b[i]/A[i][i];
+        v[i] = 0;
+    }
+    for(int k=1;k<interMax;k++){
+            cout << "\n\t" << k << " Interacao: ";
+            for(int i=0;i<tam;i++){
+                cout << x[i] << "|";
+            }
+        for(int i = 0; i<tam; i++){
+            double S = 0;
+            for(int j=0; j<tam; j++){
+                if(i != j){
+                    S += A[i][j] * x[j];
+                }
+            }
+            //cout << "\n" << b[i] << "-" << S << "/" << A[i][i] << "=" << ((b[i] - S) / A[i][i]);
+            x[i] = (double) ((b[i] - S) / A[i][i]);
+        }
+        double d = parada(x, v, tam);
+        if (d <= e){
+                cout << endl;
+            for(int i=0;i<tam;i++){
+                cout << x[i] << "|";
+            }
+            return;
+        }
+
+        for(int i=0;i<tam;i++){
+            v[i] = x[i];
+        }
+    }
+
+
+}
+
+
 int main(){
 
     int tam;
     int op;
-    float equacao[ordemMax][ordemMax] = {{3,-1,1},{2,5,-1},{1,2,5}};
-    float igualdade[ordemMax] = {5,-8,-3};
+    double equacao[ordemMax][ordemMax] = {{3,-1,1},{2,5,-1},{1,2,5}};
+    double igualdade[ordemMax] = {5,-8,-3};
     float erro = 0.05;
 
     do{
@@ -146,17 +147,17 @@ int main(){
         cout << "\nQuantidade de elementos da equacao: ";
         cin >> tam;
 
-        //lerMatriz(equacao, tam);
+        lerMatriz(equacao, tam);
 
         if(converge(equacao, tam)){
             cout << "\nConverge";
 
             cout << "\n\tResultados: ";
-            //lerVetor(igualdade, tam);
+            lerVetor(igualdade, tam);
 
             imprimirSistema(equacao, igualdade, tam);
 
-            //lerErro(&erro);
+            lerErro(&erro);
 
             gaussSeidel(equacao, igualdade, erro ,tam);
 
